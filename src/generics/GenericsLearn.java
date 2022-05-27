a254485ef595c0a086bcdda4151e489f60c04ccb
Diegocastro21 <Diegocamilocastrooliveros@gmail.com>
2022-05-27 11:49:11 -0500

    practicando con generics

--- src/generics/GenericsLearn.java
+++ src/generics/GenericsLearn.java
@@ -17,6 +17,46 @@ public class GenericsLearn {
         displayArray(stringArray);
         displayArray(booleanArray);
 
+
+       /* MyIntegerClass myInt = new MyIntegerClass(1);
+        MyDoubleClass myDouble = new MyDoubleClass(3.14);
+        MyCharacterClass myChar = new MyCharacterClass('@');
+        MyStringClass myString = new MyStringClass("Aprendiendo usar generics en clases");
+*/
+
+        /*System.out.println(myInt.getValue());
+        System.out.println(myChar.getValue());
+        System.out.println(myDouble.getValue());
+        System.out.println(myString.getValue());
+        */
+
+
+        /*
+        * Todo lo anterior con las clases se puede hacer
+        * usando los generics
+        * de esta manera
+        * */
+
+
+        MyGenericClass<Integer> myInt = new    MyGenericClass<>(1);
+        MyGenericClass<Double> myDouble = new  MyGenericClass<>(3.14);
+        MyGenericClass<Character> myChar = new MyGenericClass<>('@');
+        MyGenericClass<String> myString = new  MyGenericClass<>("Aprendiendo usar generics en clases");
+
+
+        /*
+        *
+        * Tambien se puede usar de manera doble ejemplo:
+        * MyGenericClass<Integer, Integer> myInt = new MyGenericClass<>(1, 5);
+        * pero para esto desde la clase MyGenericClass tenemos que indicarle
+        * <Thing, Thing2>
+        */
+        System.out.println(myInt.getValue());
+        System.out.println(myChar.getValue());
+        System.out.println(myDouble.getValue());
+        System.out.println(myString.getValue());
+
+
     }
 
     public static <Thing> void displayArray(Thing[] array){
